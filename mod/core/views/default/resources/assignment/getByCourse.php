868074e438cc6cd9c43da081c31105559f19f082
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2024-03-01 09:30:06 -0400

    Create getByCourse.php

---
+++ mod/core/views/default/resources/assignment/getByCourse.php
@@ -0,0 +1,17 @@
+<?php
+
+include elgg_get_plugins_path()."Core/lib/utilities.php";
+
+
+$coursecode = (string) elgg_extract('courseCode', $vars);
+$title = "Assignments for ".$coursecode;
+$vars = array();
+$assignments = array();
+$assignments = getAssignmentsByCourseCode($coursecode);
+$vars['assignments'] = $assignments;
+$vars['course'] = getCourseRunByCode($coursecode);
+$content = elgg_view('Core/assignments/getByCourse', $vars);
+$vars['content'] = $content;
+$body = elgg_view_layout('one_sidebar', $vars);
+echo elgg_view_page($title, $body);
+?> 
