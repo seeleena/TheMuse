c127a0d4f61c819023ddf5576c739ad83e9f2bd4
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2024-03-12 07:16:26 -0400

    Create viewDetails.php

---
+++ mod/core/views/default/resources/assignment/viewDetails.php
@@ -0,0 +1,15 @@
+<?php
+
+include elgg_get_plugins_path()."Core/lib/utilities.php";
+
+$title = "Assignment Details";
+$id = (string) elgg_extract('assignID', $vars);
+$vars = array();
+$details = getAssignmentDetails($id);
+$vars['details'] = $details;
+//$vars['course']= getCourseRunByID($details->courseRunID);
+$content = elgg_view('Core/assignments/viewDetails', $vars);
+$vars['content'] = $content;
+$body = elgg_view_layout('one_sidebar', $vars);
+echo elgg_view_page($title, $body);
+?>
