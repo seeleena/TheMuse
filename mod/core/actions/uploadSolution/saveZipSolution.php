2cb9e1a860dff0ca9db3447c9cfddf0095d6a116
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2024-03-12 19:41:54 -0400

    Update saveZipSolution.php

--- mod/core/actions/uploadSolution/saveZipSolution.php
+++ mod/core/actions/uploadSolution/saveZipSolution.php
@@ -1,5 +1,12 @@
 <?php
+
 include elgg_get_plugins_path()."Core/lib/fileUpload.php"; 
+include elgg_get_plugins_path()."Core/lib/dbConnection.php"; 
+$mysqli = get_CoreDB_link("mysqli");
+
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 
 system_message("Your file has been uploaded.");
 
