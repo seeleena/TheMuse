2af7f961e7acf482c75100399ce8dba8f49c256e
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2024-03-12 19:43:02 -0400

    Create uploadZipSolution.php

---
+++ mod/core/views/default/resources/uploadZipSolution.php
@@ -0,0 +1,20 @@
+<?php
+
+$title = "Upload Zipped Solution";
+$vars = array();
+$content = elgg_view('Core/myCreativeProcess/uploadZipSolution', $vars);
+$vars['content'] = $content;
+$body = elgg_view_layout('one_sidebar', $vars);
+echo elgg_view_page($title, $body);
+
+/*include elgg_get_plugins_path()."Core/lib/fileUpload.php"; 
+
+system_message("Your file has been uploaded.");
+
+$zipFileGuid = uploadFile('groupSolution');
+$assignmentID = get_input('assignID');
+$groupID = getGroupID($assignmentID, $userID);
+
+storeFileGuidForGroupSolution($groupID, $assignmentID, $zipFileGuid);
+*/
+?>
