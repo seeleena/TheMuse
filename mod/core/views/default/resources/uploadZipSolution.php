1ccadfee1e84dcedcfbc448ee2c8ccbd69b6764a
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2024-03-18 16:26:37 -0400

    Update uploadZipSolution.php

--- mod/core/views/default/resources/uploadZipSolution.php
+++ mod/core/views/default/resources/uploadZipSolution.php
@@ -1,7 +1,10 @@
 <?php
 
+include elgg_get_plugins_path()."Core/lib/utilities.php";
+
 $title = "Upload Zipped Solution";
 $vars = array();
+$vars['assignID'] = $assignID;
 $content = elgg_view('Core/myCreativeProcess/uploadZipSolution', $vars);
 $vars['content'] = $content;
 $body = elgg_view_layout('one_sidebar', $vars);
