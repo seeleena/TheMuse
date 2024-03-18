894e0ccbb92af08a0c4a373047376caf7a2ac398
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2024-03-18 16:29:06 -0400

    Update uploadZipSolution.php

--- mod/core/views/default/Core/myCreativeProcess/uploadZipSolution.php
+++ mod/core/views/default/Core/myCreativeProcess/uploadZipSolution.php
@@ -1,9 +1,4 @@
-<?php
-echo elgg_view_form('uploadAssignmentSolution', array('enctype' => 'multipart/form-data'));
-?>
-
-
-/* <div class="elgg-main elgg-body">
+<div class="elgg-main elgg-body">
     <ul class="elgg-menu elgg-breadcrumbs"><li>Upload Solution</li></ul>
     <div class="elgg-head clearfix">
         <h2 class="elgg-heading-main">Upload Assignment Solution</h2>
@@ -16,4 +11,6 @@ echo elgg_view_form('uploadAssignmentSolution', array('enctype' => 'multipart/fo
 <?php
 echo elgg_view_form('uploadAssignmentSolution', array('enctype' => 'multipart/form-data'));
 ?>
-</div> */
+</div> 
+
+
