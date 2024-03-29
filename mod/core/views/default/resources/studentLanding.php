e92b31580290de952f058d6fd0fa5e36be3c6ff0
Faith Rose <70714890+F-rose@users.noreply.github.com>
2024-03-29 16:10:16 -0400

    Update studentLanding.php

--- mod/core/views/default/resources/studentLanding.php
+++ mod/core/views/default/resources/studentLanding.php
@@ -6,5 +6,6 @@ $vars = array();
 $content = elgg_view('Core/student/landing', $vars);
 $vars['content'] = $content;
 $body = elgg_view_layout('one_sidebar', $vars);
+elgg_extend_view('Core/student/landing', 'Core/myCss/custom');
 echo elgg_view_page($title, $body);
 ?>
