78dcefe91157964208fc82f059c61077f9f2ecff
Seeleena Mohammed <98282954+seeleena@users.noreply.github.com>
2024-03-30 22:20:15 -0400

    Update feedbackDashboard.php

--- mod/core/views/default/resources/feedbackDashboard.php
+++ mod/core/views/default/resources/feedbackDashboard.php
@@ -3,7 +3,12 @@
 include elgg_get_plugins_path()."Core/lib/utilities.php";
 
 $title = "Student Feedback";
-$vars['creativityFactors'] = $creativityFactors;
+
+$vars = array();
+$currentUser = elgg_get_logged_in_user_entity();
+$studentELGGID = $currentUser->guid;
+$assignmentID = 1;
+$vars['creativityFactors'] = getCreativityFactors($assigmentID, $currentUser, $studentELGGID);
 $content = elgg_view('Core/myCreativeProcess/feedbackDashboard', $vars);
 $vars['content'] = $content;
 $body = elgg_view_layout('one_sidebar', $vars);
