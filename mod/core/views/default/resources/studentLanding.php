6bae1483b8881edeebbcbe363da4f15eb84c0e95
Faith Rose <70714890+F-rose@users.noreply.github.com>
2024-04-17 15:02:41 -0400

    Update studentLanding.php

--- mod/core/views/default/resources/studentLanding.php
+++ mod/core/views/default/resources/studentLanding.php
@@ -1,11 +1,10 @@
 <?php
 
-$title = "Student Menu";
+$title = "Student Menu";//Page header
 $vars = array();
 
-$content = elgg_view('Core/student/landing', $vars);
+$content = elgg_view('Core/student/landing', $vars);// Calls the student landing view and stores it into content variable
 $vars['content'] = $content;
-$body = elgg_view_layout('one_sidebar', $vars);
-elgg_extend_view('Core/student/landing', 'Core/myCss/custom');
-echo elgg_view_page($title, $body);
+$body = elgg_view_layout('one_sidebar', $vars);// puts content into a layout and body variable
+echo elgg_view_page($title, $body);// displays heading and content of page
 ?>
